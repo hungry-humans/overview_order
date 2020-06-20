c76c5e469e4e1517aa856a71661d16439e8e04db
JoonPark <zxqw1104@gmail.com>
2020-06-19 17:52:32 -0700

    Implement API for business overview
    Implement route 'search/:biz_id' to get business information
      return res.json from Server Side

--- server/server.js
+++ server/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+const bodyparser = require('body-parser');
+
+//db for business
+const bizdb = require('./database/db_biz');
+
+
+app.use(cors());
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 app.use(express.static('public'));
 
-app.get('/search', (req, res) => {
-  console.log('testing get request');
-  res.end();
+
+// Checking if API uses biz_id
+app.param('biz_id', (req, res, next, id) => {
+  console.log('API_business information is request');
+  next();
+});
+
+//API business info - for overview
+app.get('/search/:biz_id', (req, res) => {
+  bizdb.sync()
+    .then(()=> bizdb.findOne({where:{biz_id: req.params.biz_id}}))
+    .then((business) => res.status(200).json(business))
+    .finally(() => res.end() );
 });
 
 
